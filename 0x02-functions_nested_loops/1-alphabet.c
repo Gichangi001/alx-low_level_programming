2d5daccad55b9a493d990fe955854acfb8893ac0
Gichangi001 <alexandergichangi@gmail.com>
2023-06-20 11:20:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ * followed by a new line. #44
+ * Return: always zero if code was success
+ */
+
+void print_alphabet(void)
+
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+
+}
